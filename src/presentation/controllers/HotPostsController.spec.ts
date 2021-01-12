a4e8f18e4ab1c583bbe3990011c4c9c698c23169
Bruno Giminiani <giminiani@gmail.com>
2021-01-12 13:11:26 -0300

    test: ensure HotPostsController return status code 400 if initial date is invalid

--- src/presentation/controllers/HotPostsController.spec.ts
+++ src/presentation/controllers/HotPostsController.spec.ts
@@ -1,13 +1,30 @@
 import { HotPostsController } from './HotPostsController'
 import { MissingParamError } from '../errors/MissingParamError'
+import { InvalidParamError } from '../errors/InvalidParamError'
+import { IDateValidator } from '../protocols/IDateValidator'
 
-const makeSut = (): HotPostsController => {
-  return new HotPostsController()
+interface ISut {
+  sut: HotPostsController
+  dateValidatorStub: IDateValidator
+}
+
+const makeSut = (): ISut => {
+  class DateValidatorStub implements IDateValidator {
+    isValid (date: string): boolean {
+      return true
+    }
+  }
+  const dateValidatorStub = new DateValidatorStub()
+  const sut = new HotPostsController(dateValidatorStub)
+  return {
+    dateValidatorStub,
+    sut
+  }
 }
 
 describe('HotPostsController', () => {
   it('Should return status code 400 if initial date is not provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         finalDate: 'final_date',
@@ -20,7 +37,7 @@ describe('HotPostsController', () => {
   })
 
   it('Should return status code 400 if final date is not provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         initialDate: 'initial_date',
@@ -33,7 +50,7 @@ describe('HotPostsController', () => {
   })
 
   it('Should return status code 400 if order is not provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         initialDate: 'initial_date',
@@ -43,5 +60,20 @@ describe('HotPostsController', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('order'))
+  })
+
+  it('Should return status code 400 if initial date is invalid', () => {
+    const { sut, dateValidatorStub } = makeSut()
+    jest.spyOn(dateValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        initialDate: 'invalid_initial_date',
+        finalDate: 'final_date',
+        order: 'ups'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('initialDate'))
   })
 })
