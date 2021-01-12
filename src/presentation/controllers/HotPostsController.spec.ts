29c054c064b38dc027e0faaeecf18c08f61b6904
Bruno Giminiani <giminiani@gmail.com>
2021-01-12 05:40:37 -0300

    test: ensure HotPostsController returns status code 400 if order is not provided

--- src/presentation/controllers/HotPostsController.spec.ts
+++ src/presentation/controllers/HotPostsController.spec.ts
@@ -27,4 +27,17 @@ describe('HotPostsController', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('finalDate'))
   })
+
+  it('Should return status code 400 if order is not provided', () => {
+    const sut = new HotPostsController()
+    const httpRequest = {
+      body: {
+        initialDate: 'initial_date',
+        finalDate: 'final_date'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('order'))
+  })
 })
