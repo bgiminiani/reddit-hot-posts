d52402036c945dd3453b13ecf9daa04b00034faa
Bruno Giminiani <giminiani@gmail.com>
2021-01-12 05:49:26 -0300

    refactor: make HotPostsController implements the controller interface

--- src/presentation/controllers/HotPostsController.ts
+++ src/presentation/controllers/HotPostsController.ts
@@ -1,8 +1,9 @@
 import { IHttpRequest, IHttpResponse } from '../protocols/IHttp'
 import { MissingParamError } from '../errors/MissingParamError'
 import { badRequest } from '../helpers/HttpHelper'
+import { IController } from '../protocols/IController'
 
-export class HotPostsController {
+export class HotPostsController implements IController {
   handle (httpRequest: IHttpRequest): IHttpResponse {
     const requiredParams = ['initialDate', 'finalDate', 'order']
     for (const param of requiredParams) {
---
+++ src/presentation/protocols/IController.ts
@@ -0,0 +1,5 @@
+import { IHttpRequest, IHttpResponse } from '../protocols/IHttp'
+
+export interface IController {
+  handle: (httpRequest: IHttpRequest) => IHttpResponse
+}
