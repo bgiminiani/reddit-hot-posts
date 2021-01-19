aaea4015f9bf4cc17878ce2cb5d968af11bedd6b
Bruno Giminiani <giminiani@gmail.com>
2021-01-19 19:46:27 -0300

    refactor: change DateValidator creation to a factory method

--- src/utils/DateValidatorAdapter.spec.ts
+++ src/utils/DateValidatorAdapter.spec.ts
@@ -7,22 +7,26 @@ jest.mock('validator', () => ({
   }
 }))
 
+const makeSut = (): DateValidatorAdapter => {
+  return new DateValidatorAdapter()
+}
+
 describe('DateValidator Adapter', () => {
   it('Should return false if validator returns false', () => {
-    const sut = new DateValidatorAdapter()
+    const sut = makeSut()
     jest.spyOn(validator, 'isDate').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_date')
     expect(isValid).toBe(false)
   })
 
   it('Should return true if validator returns true', () => {
-    const sut = new DateValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('valid_date')
     expect(isValid).toBe(true)
   })
 
   it('Should call validator with correct date', () => {
-    const sut = new DateValidatorAdapter()
+    const sut = makeSut()
     const isDateSpy = jest.spyOn(validator, 'isDate')
     sut.isValid('2018-10-03 03:48:21')
     expect(isDateSpy).toHaveBeenCalledWith('2018-10-03 03:48:21')
