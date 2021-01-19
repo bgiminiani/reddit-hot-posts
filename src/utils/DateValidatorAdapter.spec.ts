77fd867cd83e8a4842da6c4579427d34c64903eb
Bruno Giminiani <giminiani@gmail.com>
2021-01-19 16:20:08 -0300

    test: ensure DateValidatorAdapter returns false if validator returns false

--- src/utils/DateValidatorAdapter.spec.ts
+++ src/utils/DateValidatorAdapter.spec.ts
@@ -3,7 +3,7 @@ import { DateValidatorAdapter } from './DateValidatorAdapter'
 describe('DateValidator Adapter', () => {
   it('Should return false if validator returns false', () => {
     const sut = new DateValidatorAdapter()
-    const isValid = sut.isValid('00/1/00')
+    const isValid = sut.isValid('invalid_date')
     expect(isValid).toBe(false)
   })
 })
