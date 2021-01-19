6745e4f38e1ffaf2e65a57358c11726d4aa6a3c9
Bruno Giminiani <giminiani@gmail.com>
2021-01-19 17:20:11 -0300

    test: ensure DateValidatorAdapter returns true if validator returns true

--- src/utils/DateValidatorAdapter.spec.ts
+++ src/utils/DateValidatorAdapter.spec.ts
@@ -1,9 +1,23 @@
+import validator from 'validator'
 import { DateValidatorAdapter } from './DateValidatorAdapter'
+
+jest.mock('validator', () => ({
+  isDate (): boolean {
+    return true
+  }
+}))
 
 describe('DateValidator Adapter', () => {
   it('Should return false if validator returns false', () => {
     const sut = new DateValidatorAdapter()
+    jest.spyOn(validator, 'isDate').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_date')
     expect(isValid).toBe(false)
+  })
+
+  it('Should return true if validator returns true', () => {
+    const sut = new DateValidatorAdapter()
+    const isValid = sut.isValid('valid_date')
+    expect(isValid).toBe(true)
   })
 })
