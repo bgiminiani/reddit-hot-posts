dfb7db5d46d3ed52529d0d88c34108f30e16756a
Bruno Giminiani <giminiani@gmail.com>
2021-01-20 16:38:43 -0300

    test: ensure DBLoadHotPosts returns hotposts on success

--- src/data/usecases/load-hotposts/DBLoadHotPosts.spec.ts
+++ src/data/usecases/load-hotposts/DBLoadHotPosts.spec.ts
@@ -1,3 +1,4 @@
+import MockDate from 'mockdate'
 import { DBLoadHotPosts } from './DBLoadHotPosts'
 import { IHotPostsParam } from '../../../domain/usecases/load-hotposts'
 import { IHotPost } from '../../../domain/models/IHotPost'
@@ -47,6 +48,14 @@ const makeSut = (): ISut => {
 }
 
 describe('DBLoadHotPosts Usecaes', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
   it('Should call LoadHotPostsRepository with correct values', async () => {
     const { sut, loadHotPostsRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadHotPostsRepositoryStub, 'load')
@@ -69,5 +78,35 @@ describe('DBLoadHotPosts Usecaes', () => {
     }
     const hotPostsPromises = sut.load(hotPostsParams)
     await expect(hotPostsPromises).rejects.toThrow()
+  })
+
+  it('Should return hot posts on success', async () => {
+    const { sut } = makeSut()
+    const hotPostsParams = {
+      initialDate: 'valid_initial_date',
+      finalDate: 'valid_final_date',
+      orderBy: 'valid_order_by'
+    }
+    const hotPosts = await sut.load(hotPostsParams)
+    expect(hotPosts).toEqual([
+      {
+        id: '39bf55ee-e55c-40e1-9539-d57d2bf53eed',
+        postTitle: 'Welcome to /r/artificial!',
+        authorName: 't2_3dncp',
+        authorFullName: 'CyberByte',
+        creationDate: new Date(),
+        numberOfUps: 130,
+        numberOfComments: 16
+      },
+      {
+        id: '3007c85a-d708-43ca-ba5d-8738052ccbd1',
+        postTitle: 'Google AI Introduces ToTTo: A Controlled Table-to-Text Generation Dataset Using Novel Annotation Process',
+        authorName: 't2_2wsvqwhg',
+        authorFullName: 'ai-lover',
+        creationDate: new Date(),
+        numberOfUps: 23,
+        numberOfComments: 1
+      }
+    ])
   })
 })
